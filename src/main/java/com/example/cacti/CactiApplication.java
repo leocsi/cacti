13ab6f76a0c450f6985f55951074da7d2a06e5b8
MichaelAppel <appelmichal@gmail.com>
2023-09-22 09:32:35 +0000

    new Main

--- src/main/java/com/example/cacti/CactiApplication.java
+++ src/main/java/com/example/cacti/CactiApplication.java
@@ -3,11 +3,14 @@ package com.example.cacti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CactiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CactiApplication.class, args);
+		Arrays.stream(SpringApplication.run(CactiApplication.class, args)
+				.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
